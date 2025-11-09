55dc32f836f6d580df010f9e7348181d8e7d962b
Eurymachus <eury@machus.co.uk>
2025-11-09 17:24:49 +0000

    Removed unnecessary Debug spam from Options

--- Contents/mods/TransmogDefinitiveEdition/42/media/lua/client/Transmog/Options.lua
+++ Contents/mods/TransmogDefinitiveEdition/42/media/lua/client/Transmog/Options.lua
@@ -43,24 +43,24 @@ end
 -- Helpers: treat getValue() as boolean; guard against nil
 function OPS.shouldHideDirt()
     local result = OPS.hideDirt and OPS.hideDirt:getValue() == true
-    TmogPrint("shouldHideDirt: " .. tostring(result))
+    -- TmogPrint("shouldHideDirt: " .. tostring(result))
     return result
 end
 
 function OPS.shouldHideBlood()
     local result = OPS.hideBlood and OPS.hideBlood:getValue() == true
-    TmogPrint("shouldHideBlood: " .. tostring(result))
+    -- TmogPrint("shouldHideBlood: " .. tostring(result))
     return result
 end
 
 function OPS.shouldHideHoles()
     local result = OPS.hideHoles and OPS.hideHoles:getValue() == true
-    TmogPrint("shouldHideHoles: " .. tostring(result))
+    -- TmogPrint("shouldHideHoles: " .. tostring(result))
     return result
 end
 
 function OPS.shouldHidePatches()
     local result = OPS.hidePatches and OPS.hidePatches:getValue() == true
-    TmogPrint("shouldHidePatches: " .. tostring(result))
+    -- TmogPrint("shouldHidePatches: " .. tostring(result))
     return result
 end
